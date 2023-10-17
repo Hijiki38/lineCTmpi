ed521ad45c04bcd635eae4a2d05734fc37337604
Takumi Hotta <takumi-h@zodiacz.com>
2023-10-17 21:10:58 +0900

    modified parameter.py

--- gcp_client/parameter.py
+++ gcp_client/parameter.py
@@ -1,14 +1,14 @@
 # GCP用パラメータ
 zone = 'us-central1-b'	# インスタンスグループを作成したZONE
 instance_group_name = 'linectmpi'	# インスタンスグループ名
-user_name = "takumimh"   # インスタンスで作成した共有ユーザ名
+user_name = "takumi_h"   # インスタンスで作成した共有ユーザ名
 repository_name = "lineCTmpi" # git cloneしたリポジトリ名
 num_instance = 3	# 同時実行するインスタンス数
 poling_timer = 10	# 処理待ち時の待機時間(sec)
 
 # 計算用パラメータ
-par_sod = 11.6	# 線源ー被写体間距離(cm)
-par_sdd = 50	# 線源ー検出器間距離(cm)
+par_sod = 40	# 線源ー被写体間距離(cm)
+par_sdd = 80	# 線源ー検出器間距離(cm)
 par_ptch = 0.01	# ピクセルの大きさ(cm)
 par_ttms = 128	# ピクセル数
 par_step = 4 # 1440	# 投影数
@@ -22,5 +22,5 @@ par_beam = 1	# ビーム(0:Parallel, 1:Fan)
 calc_dir_path = f'/home/{user_name}/{repository_name}/core/' # dockerを起動し計算を行うディレクトリのパス（"/"まで）
 share_dir_path = f'/home/{user_name}/{repository_name}/core/share/' # 計算結果を出力するディレクトリのパス（"/"まで）
 gdrive_dir_path = f'/home/{user_name}/{repository_name}/gcp_VM/' # upload.py と mergecsv.pyが格納されたディレクトリのパス（"/"まで）
-keyfile_path = f'/home/{user_name}/{repository_name}/linectmpi-fcfdc9557818.json' # サービスアカウントキーのパス
+keyfile_path = f'/home/{user_name}/{repository_name}/linectmpi-401502-711efe271615.json' # サービスアカウントキーのパス
 share_drive_id = '1pQ5akiTWsCuqtgw3ZbTBQFIR_xmvvp1L' # アップロード先のフォルダID
